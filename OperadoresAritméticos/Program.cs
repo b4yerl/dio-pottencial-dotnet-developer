08a43ee47109d61c0b14160904d04b1ee7103c4d
b4yerl <gcbayerlc@gmail.com>
2022-09-20 19:19:39 -0300

    Tipos de operadores em C#

---
+++ OperadoresAritméticos/Program.cs
@@ -0,0 +1,9 @@
+﻿using OperadoresAritméticos.Models;
+
+Calculadora teste = new Calculadora();
+
+// teste.Seno(30);
+// teste.Cosseno(30);
+// teste.Tangente(30);
+
+teste.RaizQuadrada(5);
---
+++ OperadoresAtribuicao/Program.cs
@@ -0,0 +1,84 @@
+﻿// int a = 2;
+// int b = 3;
+// int c = a + b;
+
+// for(int i = 0; i < b; i++) {
+//   Console.WriteLine(c *= a);
+// }
+
+//AULA 3
+
+// A principal diferença entre Convert e Parse está
+// na aceitação de valores nulos
+// int a = Convert.ToInt32("5");
+// int a = int.Parse("5");
+
+// int inteiro = 5;
+// string a = inteiro.ToString();
+// Console.WriteLine(a);
+
+// int a = 5;
+// double b = a;
+// Console.WriteLine(b);
+
+/* 
+---------------------------------------------
+  OPERADORES CONDICIONAIS
+---------------------------------------------
+*/
+
+/* int quantidadeEstoque = 10;
+int quantidadeCompra = 4;
+
+if (quantidadeEstoque >= quantidadeCompra) {
+  Console.WriteLine("Compra realizada com sucesso");
+  quantidadeEstoque -= quantidadeCompra;
+}
+
+else {
+  Console.WriteLine("Desculpa, não temos essa quantidade disponível no estoque");
+}
+
+Console.WriteLine($"Restam ainda {quantidadeEstoque} unidades no estoque");
+
+return 0; */
+/*
+Console.WriteLine("Você deseja calcular o seu IMC? (y/n)");
+string escolha = Console.ReadLine();
+
+if (escolha == "y") {
+  double peso = 0;
+  double altura = 0;
+  double imc = 0;
+
+  Console.WriteLine("Insira o seu peso: ");
+  peso = Convert.ToDouble(Console.ReadLine());
+  Console.WriteLine("Insira a sua altura: ");
+  altura = Convert.ToDouble(Console.ReadLine());
+
+  imc = peso / (altura * altura);
+  imc = Math.Round(imc, 2);
+  Console.WriteLine($"O seu IMC é {imc}");
+  return 0;
+}
+else {
+  return 0;
+}*/
+/*
+Console.WriteLine("Digite uma letra");
+string letra = Console.ReadLine();
+
+switch (letra)
+{
+  case "a":
+  case "e":
+  case "i":
+  case "o":
+  case "u":
+    Console.WriteLine("Vogal");
+    break;
+  default: 
+    Console.WriteLine("Não é uma vogal");
+    break;
+}*/
+
