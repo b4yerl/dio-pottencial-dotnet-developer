c869ac5734c0888cc6bc1224c276ead1ce5d72d5
b4yerl <gcbayerlc@gmail.com>
2022-09-22 19:44:13 -0300

    Fizalizada a primeira parte dos "Fundamentos do C#"

---
+++ IntroCSharp/ArraysListas/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] testeArray = new int[3];
+
+testeArray[0] = 1921;
+testeArray[1] = 1942;
+testeArray[2] = 2022;
+
+// for(int i = 0; i < testeArray.Length; i++) {
+// Console.WriteLine(testeArray[i]);
+// }
+
+// foreach (int valor in testeArray)
+// {
+//   Console.WriteLine(valor);
+// }
+
+int[] arrayDobrado = new int[testeArray.Length * 2];
+
+// for(int i = 0; i < testeArray.Length; i++) {
+//   arrayDobrado[i] = testeArray[i];
+//   Console.WriteLine($"O item #{i + 1} do novo array, também é {arrayDobrado[i]}");
+// }
+
+Array.Copy(testeArray, arrayDobrado, testeArray.Length);
+for(int i = 0; i < arrayDobrado.Length; i++) {
+  Console.WriteLine(arrayDobrado[i]);
+}
---
+++ IntroCSharp/EstruturasRepeticao/Program.cs
@@ -0,0 +1,58 @@
+﻿// Console.WriteLine("--POTÊNCIA--");
+// Console.WriteLine("Digite um inteiro: ");
+// int numeroBase = Convert.ToInt32(Console.ReadLine());
+
+// for(int i = 0; i < 10; i++){
+//   Console.WriteLine($"{numeroBase} elevado a {i+1} = {Math.Pow(numeroBase, (i + 1))}");
+// }
+
+// int i = 0;
+// while (i < 10) {
+//   Console.WriteLine($"{numeroBase} elevado a {i+1} = {Math.Pow(numeroBase, (i + 1))}");
+//   i++;
+// }
+
+// int soma = 0;
+// int novoNumero = 0;
+// Console.WriteLine("Digite os inteiros a serem somados. Quando estiver pronto, digite 0.");
+// do {
+//   novoNumero = Convert.ToInt32(Console.ReadLine());
+//   soma += novoNumero;
+// } while(novoNumero != 0);
+// Console.WriteLine($"A soma é {soma}");
+
+string opcao;
+bool condicaoSaida = true;
+
+while (condicaoSaida)
+{
+  Console.WriteLine("Digite a sua opção: ");
+  Console.WriteLine("1 - Cadastrar Cliente");
+  Console.WriteLine("2 - Buscar Cliente");
+  Console.WriteLine("3 - Apagar Cleinte");
+  Console.WriteLine("4 - Encerrar\n");
+
+  opcao = Console.ReadLine();
+
+  switch (opcao)
+  {
+    case "1":
+      Console.WriteLine("Cadastro de Cliente\n");
+      break;
+    case "2":
+      Console.WriteLine("Busca de Cliente\n");
+      break;
+    case "3": 
+      Console.WriteLine("Apagar Cliente\n");
+      break;
+    case "4":
+      Console.WriteLine("Operação encerrada\n");
+      condicaoSaida = false;
+      break;
+    default:
+      Console.WriteLine("Opção Inválida\n");
+      break;    
+  }
+}
+
+Console.WriteLine("flwwww");
